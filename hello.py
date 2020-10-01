c3ca64aadb43fd0f866a944160db42f2ccff0f42
Noah Gift <noah.gift@gmail.com>
2020-10-01 15:36:35 +0000

    adding test code

--- hello.py
+++ hello.py
@@ -1,6 +1,20 @@
-#var=
-def speak(name):
-    return "Hello, my name is %s" % name
+import click
 
-print(speak("Bob"))
-print("This is fun!")
+def logic(name):
+    if name == "Marco":
+        return "Polo"
+    else:
+        return "No Match"
+
+@click.command()
+@click.option("--name")
+def marco(name):
+    """This is a Marco Polo function"""
+    
+    result = logic(name)
+    click.echo(result)
+
+
+if __name__ == "__main__":
+    # pylint: disable=no-value-for-parameter
+    marco()
--- test_hello.py
+++ test_hello.py
@@ -1,5 +1,21 @@
-from hello import speak
+from hello import logic
+from click.testing import CliRunner
+from hello import marco
 
+def test_marco_polo():
+  runner = CliRunner()
+  result = runner.invoke(marco, ['--name', 'Marco'])
+  assert result.exit_code == 0
+  assert 'Polo' in result.output
+  
+def test_marco_bob():
+  runner = CliRunner()
+  result = runner.invoke(marco, ['--name', 'Bob'])
+  assert result.exit_code == 0
+  assert 'No Match' in result.output
 
-def test_speak():
-    assert "bob" in speak("bob")
+def test_logic():
+    assert "Polo" in logic("Marco")
+    assert "No Match" == logic("Bob")
+    
+    
