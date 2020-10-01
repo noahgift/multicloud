334629bea3f838d52e5ea16ba4d578e3b82db9dd
Noah Gift <noah.gift@gmail.com>
2020-10-01 15:44:41 +0000

    adding new functionality

--- Makefile
+++ Makefile
@@ -2,9 +2,6 @@ install:
 	pip install --upgrade pip &&\
 		pip install -r requirements.txt
 
-format:
-	black *.py
-
 lint:
 	pylint --disable=R,C hello.py
 
--- hello.py
+++ hello.py
@@ -12,7 +12,12 @@ def marco(name):
     """This is a Marco Polo function"""
     
     result = logic(name)
-    click.echo(result)
+    click.echo(click.style('Checking Marco Polo', fg='green'))
+    if result == "Polo":
+        click.echo(click.style(f"{result}",bg='blue', fg='red' ))
+    else:
+        click.echo(click.style(f"ATTENTION! Wrong Name: {result}", 
+            blink=True, bold=True))
 
 
 if __name__ == "__main__":
